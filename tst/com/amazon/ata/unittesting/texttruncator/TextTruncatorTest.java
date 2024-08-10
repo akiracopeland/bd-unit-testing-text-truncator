03f8669f49ac7c8b453f1aa58d0cf98dbccaabca
akiracopeland <akiracopeland713@gmail.com>
2024-08-09 21:47:14 -0700

    Text Truncator Quiz done

--- tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
+++ tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
@@ -22,6 +22,50 @@ public class TextTruncatorTest {
 
     }
 
+    @Test
     /* ADD YOUR TESTS HERE */
+    public void truncateTo_givenNullString_returnsEmptyString() {
+        // GIVEN
+        String original = null;
+        TextTruncator truncator = new TextTruncator(original);
+
+        // WHEN
+        String truncatedString = truncator.truncateTo(10);
+
+        // THEN
+        Assertions.assertEquals("", truncatedString,
+                "Expected to return an empty string");
+
+    }
+
+    @Test
+    public void truncateTo_stringLongerThan_toProperLength() {
+        // GIVEN
+        String original = "some random string";
+        TextTruncator truncator = new TextTruncator(original);
+
+        // WHEN
+        String truncatedString = truncator.truncateTo(10);
+
+        // THEN
+        Assertions.assertEquals(10, truncatedString.length(),
+                "Expected length of 10");
+
+    }
+
+    @Test
+    public void truncateTo_stringShorterThan_returnsOriginalString() {
+        // GIVEN
+        String original = "shorter than 20";
+        TextTruncator truncator = new TextTruncator(original);
+
+        // WHEN
+        String truncatedString = truncator.truncateTo(20);
+
+        // THEN
+        Assertions.assertEquals(original, truncatedString,
+                "Expected the original string");
+
+    }
 
 }
